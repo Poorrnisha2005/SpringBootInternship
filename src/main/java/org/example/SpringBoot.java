bb2069392f33172874de9ca003437bed7b9d33d3
Poorrnisha <kumarnisha702@gmail.com>
2025-07-02 21:45:54 +0530

    Initial commit

---
+++ src/main/java/org/example/SpringBoot.java
@@ -0,0 +1,11 @@
+package org.example;
+
+public class SpringBoot {
+    public SpringBoot(){
+
+        System.out.println("Class is going in CodeStudio Lab");
+    }
+    public void hello(String name){
+        System.out.println("Hello "+name);
+    }
+}
